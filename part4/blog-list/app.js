24bf2c256eb65f99bac6cf67f4d03ba955d3f1c8
Eero Lehtinen <eero@eerolehtinen.fi>
2020-05-16 21:34:32 +0300

    part 4.2 done

---
+++ part4/blog-list/app.js
@@ -0,0 +1,31 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoose = require('mongoose')
+
+const express = require('express')
+const app = express()
+
+logger.info('Connecting to', config.MONGO_URL)
+
+mongoose.connect(config.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		logger.info('Connected to MongoDB')
+	})
+	.catch(err => {
+		logger.error('Error connecting to MongoDB:', err.message)
+	})
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,41 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
-
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+const PORT = config.PORT || 3001
+server.listen(PORT, () => {
+	logger.info(`Server running on port ${PORT}`)
 })
